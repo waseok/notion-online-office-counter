c1a534a2a064c0c1643951cc2cd800d22e015676
ddongdosa <lds43890@gmail.com>
2026-07-23 12:50:13 +0900

    Allow unrestricted embedding in Notion

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
       {
         source: "/:path*",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-ancestors 'self' https://notion.so https://www.notion.so https://*.notion.so https://*.notion.site",
-          },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           { key: "X-Content-Type-Options", value: "nosniff" },
         ],
